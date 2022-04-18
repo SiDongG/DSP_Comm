11404a4947a370c2dfb7ce74af0910fdf76f59a2
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-04-18 18:04:00 -0400

    Delete iblock_dct2.m

--- iblock_dct2.m
+++
@@ -1,9 +0,0 @@
-function X = iblock_dct2 ( a )
-[m , n ] = size ( a ) ;
-X = zeros (m , n ) ;
-for i = 1:8: m
-for j = 1:8: n
-X ( i : i +7 , j : j +7) = idct2 ( a ( i : i +7 , j : j +7) ) ;
-end
-end
-end
